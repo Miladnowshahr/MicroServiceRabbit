1ced5964f029abb9b33c4be23f08b936a61d80dd
Milad Nowshahr <miladnowshahr@yahoo.com>
2020-03-16 21:19:41 +0330

    Implementation of rabbitMq Bus completer

--- MicroRabbit/MicroRabbit.Domain.Core/Bus/IEventBus.cs
+++ MicroRabbit/MicroRabbit.Domain.Core/Bus/IEventBus.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.Threading.Tasks;
+using MicroRabbit.Domain.Core.Command;
+using MicroRabbit.Domain.Core.Event;
 namespace MicroRabbit.Domain.Core.Bus
 {
     public interface IEventBus
     {
-        Task SendCommand<T>(T command) where T : Command;
+        Task SendCommand<T>(T command) where T : Command.Command;
 
-        void Publish<T>(T @event) where T : Event;
+        void Publish<T>(T @event) where T : Event.Event;
 
-        void Subscibe<T, Th>()
-            where T : Event
+        void Subscibe<T, TH>()
+            where T : Event.Event
             where TH : IEventHandler<T>;
     }
 }
--- MicroRabbit/MicroRabbit.Domain.Core/Command/Command.cs
+++ MicroRabbit/MicroRabbit.Domain.Core/Command/Command.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MicroRabbit.Domain.Core.Event;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,7 +8,7 @@ namespace MicroRabbit.Domain.Core.Command
     public abstract class Command:Message
     {
 
-        public DateTime Timestap { get; protected set };
+        public DateTime Timestap { get; protected set; };
 
         protected Command()
         {
